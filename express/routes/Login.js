dfaa5003b53d016e3d4af92b499bab784dfc6e49
kel-lit <kel_littley@live.co.uk>
2021-04-30 17:40:58 +0100

    Modularise and add test login route to Express backend

--- express/routes/Login.js
+++ express/routes/Login.js
@@ -1,2 +1,24 @@
 const loginRouter 	= require('express').Router();
-const secrets 		= require('../secrets.json');
+const getDb 		= require('../database/database').getDb;
+
+loginRouter.post('/', (req, res) => {
+	_db = getDb();
+
+	users = _db.collection('users');
+
+	users.findOne({code: req.body.code})
+		.then(result => {
+			if (!result) {
+				res.json({"body": {"error": "Code not found"}});
+			}
+			else {
+				console.log(JSON.stringify(result.name));
+				res.json({"body": {"success": result.name}});
+			}
+		})
+		.catch(error => {
+			res.json({"error": error});
+		})
+})
+
+module.exports = loginRouter;
--- express/routes/index.js
+++ express/routes/index.js
@@ -4,5 +4,15 @@ const Login 	= require('./Login')
 const routes = require('express').Router();
 
 routes.use('/database', Database);
+routes.use('/login', Login);
+
+routes.get('/test', (req, res) => {
+	res.json({"Hello": "World!"});
+});
+
+routes.post('/test', (req, res) => {
+	res.json({"Hello": req.body.message});
+});
+
 
 module.exports = routes;
