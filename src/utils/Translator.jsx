2a5649fee49aeeb51b01bc87b797b452b1c53077
kel-lit <kel_littley@live.co.uk>
2021-04-30 17:42:41 +0100

    Improve translator name (use: import {Translator as t} from ...) to make its function clearer when imported

--- src/utils/Translator.jsx
+++ src/utils/Translator.jsx
@@ -1,7 +1,7 @@
 import English from '../languages/English.json';
 import Italian from '../languages/Italian.json';
 
-export default function t(key) {
+function Translator(key) {
 	let currentLanguage = document.cookie.split('; ').find(row => row.startsWith('language=')).split('=')[1];
 
 	if (!currentLanguage) return key;
@@ -16,3 +16,5 @@ export default function t(key) {
 
 	return result ? result : key;
 }
+
+export { Translator };
