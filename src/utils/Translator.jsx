4d9dcd100c7ef6752ff7175abeacb74c05945868
kel-lit <kel_littley@live.co.uk>
2021-09-04 15:56:15 +0100

    fix (Translator): Fixed translator sometimes returning an object causing React to crash

--- src/utils/Translator.jsx
+++ src/utils/Translator.jsx
@@ -2,20 +2,18 @@ import English from 'languages/English.json';
 import Italian from 'languages/Italian.json';
 
 function Translator(key) {
-	let currentLanguage = document.cookie.split('; ').find(row => row.startsWith('language=')).split('=')[1];
-
-	if (!currentLanguage) return key;
+	let currentLanguage = document.cookie.split('; ').find(row => row.startsWith('language=')).split('=')[1] || "english";
 
 	const 	source 		= currentLanguage == "english" ? English : Italian;
 	const 	sections 	= key.split('.');
 	let 	result 		= source;
 
-	sections.forEach(section => {
+	for (const section of sections) {
 		if (!result[section]) {
 			return key;
 		}
 		result = result[section];
-	})
+	}
 
 	return result ? result : key;
 }
