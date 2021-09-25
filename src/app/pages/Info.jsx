4265310349d8a3fb645340c320bae8398895f105
kel-lit <kel_littley@live.co.uk>
2021-09-25 20:16:55 +0100

    Feat (Information page): Add WIP content to info page

--- src/app/pages/Info.jsx
+++ src/app/pages/Info.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { PageHeading, PageContent } from '../AddOns/PageLayout'
 
 export default function(props) {
 	return (
-		<h1>Information</h1>
+		<>
+			<PageHeading value={'Information'} />
+
+			<PageContent value={
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, quidem adipisci ullam facere sapiente ipsam nesciunt harum? Exercitationem nostrum sapiente temporibus similique quisquam ad voluptatibus cumque quos tenetur. Alias, rem.'
+			} />
+
+		</>
 	)
 }
