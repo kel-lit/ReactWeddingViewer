ca1d5a2444f08c82b3f64c4322ac77801cabd807
kel-lit <kel_littley@live.co.uk>
2021-11-04 21:30:47 +0000

    Feat (Frontend): Add nicer placeholder pages

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useContext } from 'react';
-import { PageHeading, PageContent } from '../AddOns/PageLayout';
+import PageLayout, { PageHeading, PageContent, PageDivider } from '../AddOns/PageLayout';
 import NameJoiner from 'utils/NameJoiner';
 
 import { UserContext } from '../../index';
@@ -10,10 +10,12 @@ export default function Home () {
 	const context = useContext(UserContext);
 
 	return (
-		<>
-			<PageHeading value={NameJoiner(context.guests)} />
+		<PageLayout>
+			<PageHeading value={NameJoiner(context.guestInfo.guests.map(guest => guest.name))} />
 			
+			<PageDivider />
+
 			<PageContent value={'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum sapiente facere quis esse corporis libero eaque quo animi odio est aperiam omnis cumque consequatur hic saepe, id quisquam consectetur minus.'} />
-		</>
+		</PageLayout>
 	)
 }
--- src/app/pages/Info.jsx
+++ src/app/pages/Info.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import { PageHeading, PageContent } from '../AddOns/PageLayout'
+import PageLayout, { PageHeading, PageContent, PageDivider } from '../AddOns/PageLayout'
 
 export default function(props) {
 	return (
-		<>
+		<PageLayout>
 			<PageHeading value={'Information'} />
+
+			<PageDivider />
 
 			<PageContent value={
 				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, quidem adipisci ullam facere sapiente ipsam nesciunt harum? Exercitationem nostrum sapiente temporibus similique quisquam ad voluptatibus cumque quos tenetur. Alias, rem.'
 			} />
 
-		</>
+		</PageLayout>
 	)
 }
