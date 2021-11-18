34d586a185a73bc20d2df2f7d6d4007b79ac3a58
kel-lit <kel_littley@live.co.uk>
2021-11-18 09:52:15 +0000

    Feat (Location): Add translatable string for info to Location page

--- src/app/pages/Location.jsx
+++ src/app/pages/Location.jsx
@@ -1,7 +1,51 @@
 import React from 'react'
+import PageLayout, { PageHeading, PageContent, PageDivider, PageContentImage } from '../AddOns/PageLayout'
+
+import styles from './Location.scss'
+
+import imageCeremonyRoom from 'images/location-ceremony-room.jpg'
+import imageReceptionRoom from 'images/location-reception-room.jpg'
 
 export default function(props) {
 	return (
-		<h1>Location</h1>
+		<PageLayout>
+			<PageHeading value={t('pages.location.title')} />
+
+			<PageDivider />
+
+			<PageContent>
+				{t('pages.location.maincontent')}
+
+				<PageContentImage src={imageCeremonyRoom} />
+
+				{t('pages.location.locationdescription')}
+
+				<PageContentImage src={imageReceptionRoom} />
+
+				<Map />
+			</PageContent>
+		</PageLayout>
+	)
+}
+
+function Map() {
+	return (
+		<div className={styles.mapOuter}>
+			<div className={styles.mapCanvas}>
+				<iframe width={"100%"}
+					height={"400px"}
+					id={"mapCanvas"}
+					src={"https://maps.google.com/maps?q=Dodford%20Manor&t=&z=11&ie=UTF8&iwloc=&output=embed"}
+					frameBorder={"0"}
+					scrolling={"no"}
+					marginHeight={"0"}
+					marginWidth={"0"}>
+				</iframe>
+
+				<a href={"https://www.whatismyip-address.com"} />
+				<br/>
+				<a href="https://www.embedgooglemap.net" />
+			</div>
+		</div>
 	)
 }
