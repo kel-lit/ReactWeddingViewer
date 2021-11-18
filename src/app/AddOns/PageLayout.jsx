7deb709d43fbe51159d8d49e6242d085ef6b21db
kel-lit <kel_littley@live.co.uk>
2021-11-18 09:54:15 +0000

    Feat (Page Layout): Improve page layout

--- src/app/AddOns/PageLayout.jsx
+++ src/app/AddOns/PageLayout.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './PageLayout.scss'
 
@@ -8,13 +8,22 @@ function PageHeading({ value, className }) {
 	)
 }
 
-function PageContent({ value, className }){
+function PageContent({ className, children }){
 	return (
 		<div className={styles.contentContainer}>
-			<div className={styles.contentInner}>{value}</div>
+			{ React.Children.map(children, child => {
+				if (child.type == PageContentImage)
+					return <ExpandableImage className={styles.contentInnerImage} {...child.props} />
+				else 
+					return <div className={styles.contentInner}>{child}</div>
+			}) }
 		</div>
 	)
 }
+
+function PageContentImage({ src }) {
+	return <img src={src} />
+} 
 
 function PageDivider() {
 	return <div className={styles.divider}/>
@@ -26,4 +35,30 @@ export default function PageLayout({ children }) {
 	</div>
 }
 
-export { PageHeading, PageContent, PageDivider } 
+function ExpandableImage({ src, className }) {
+	const [expanded, setExpanded] = useState(false)
+
+	const visibility = expanded ? styles.visible : styles.hidden
+	const outerClasses = styles.expandedOuter + ' ' + visibility 
+	const innerClasses = styles.expandedImage + ' ' + visibility
+
+	const toggle = () => {
+		if (expanded)
+			document.body.style.overflow = "auto"
+		else
+			document.body.style.overflow = "hidden"
+
+		setExpanded(!expanded)
+	}
+
+	return (
+		<div className={styles.imageContainer}>
+			<img className={styles.expandableImage} src={src} onClick={() => toggle()} />
+			<div className={outerClasses} onClick={toggle}>
+				<img className={innerClasses} src={src}/>
+			</div>
+		</div>
+	)
+}
+
+export { PageHeading, PageContent, PageDivider, PageContentImage } 
