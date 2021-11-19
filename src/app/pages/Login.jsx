02ebc181df37b2464fd01651c684ad03e6417eec
kel-lit <kel_littley@live.co.uk>
2021-11-19 11:31:48 +0000

    Feat (Login): Improve login styles across different mobile devices

--- src/app/pages/Login.jsx
+++ src/app/pages/Login.jsx
@@ -3,27 +3,29 @@ import { BrowserRouter as Router, Redirect } from 'react-router-dom';
 import { Button } from 'utils/Buttons';
 import useJsonApi from 'utils/useJsonApi';
 import { UserContext } from '../../index';
+import MessageHandler from 'utils/MessageHandler';
 
 import styles from './Login.scss';
 
 import imageLogo from 'images/logo.png';
-import imageBackground from 'images/login-back.jpg';
 
 export default function Login(props) {
 	const [loginResponse, loginLoading, loginError, login] = useJsonApi('/api/login');
 
-	const [code, setCode] 				= useState('');
-	const [error, setError] 			= useState(null);
+	const [code, setCode] 				= useState('')
+
+	const [message, setMessage]			= useState(null)
+
 	const [inputActive, setInputActive] = useState(false);
 
 	const context = useContext(UserContext);
 
 	const doLogin = () => {
 		if (code.length < 8) {
-			setError(t('pages.login.errors.tooshort'));
+			setMessage({title: t('common.error'), message: t('pages.login.errors.tooshort'), type: 'error'});
 		}
 		else if (code.length > 8) {
-			setError(t('pages.login.errors.toolong'));
+			setMessage({title: t('common.error'), message: t('pages.login.errors.toolong'), type: 'error'});
 		}
 		else {
 			login({code: code});
@@ -39,39 +41,38 @@ export default function Login(props) {
 			context.login();
 		}
 		else if (loginResponse.result && loginResponse.result.error){
-			setError(t(loginResponse.result.error));
+			setMessage({title: t('common.error'), message: t(loginResponse.result.error), type: 'error'});
 		}
+
+
 	}, [loginResponse])
 
 	return (
 		<Router>
 			<Redirect to='/'/>
 			<div className={styles.login_body} onSubmit={(e) => { e.preventDefault(); }}>
-				<form action={null} className={styles.form}>
+				<form action={null} className={styles.form} noValidate>
 					<div className={styles.heading}>
 						<img src={imageLogo} className={styles.logo}/>	
 						<div className={styles.heading_title}>
 							{t('pages.login.title')}
 						</div>
 					</div>
-					<div className={styles.background_container}>
-						<img src={imageBackground} className={styles.background} />
-					</div>
 					<div className={styles.input_container}>
 						<div className={styles.input_card}>
-							<label htmlFor='logininput' className={styles.input_label} >{t('pages.login.codeinputlabel')}</label>
 							<input className={styles.input} 
 								onFocus={() => setInputActive(true)} 
 								onBlur={() => setInputActive(false)} 
 								onChange={(e) => setCode(e.target.value)} 
-								placeholder={!inputActive ? t('pages.login.codeinput') : ''}
-								type='url' />
+								placeholder={!inputActive ? t('pages.login.codeinputlabel') : ''}
+								type='email' />
 
-							{ error && <div className={styles.error}>{error}</div> }
 							<Button className={styles.button} onClick={doLogin} text={t('pages.login.login')} />
 						</div>
 					</div>
 				</form>
+
+				<MessageHandler message={message || {}} close={() => setMessage(null)} />
 			</div>
 		</Router>
 	)
