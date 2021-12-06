96f3cb0b8d448241bc88b3f895e1f8b479d9867f
kel-lit <kel_littley@live.co.uk>
2021-12-06 20:06:12 +0000

    Feat (Translator): Add links and newline parsing to translatable strings

--- src/utils/Translator.jsx
+++ src/utils/Translator.jsx
@@ -1,21 +1,78 @@
-import English from 'languages/English.json';
-import Italian from 'languages/Italian.json';
+import React from 'react'
+
+import English from 'languages/English.json'
+import Italian from 'languages/Italian.json'
 
 export default function Translator(key) {
-	let currentLanguage = document.cookie.split('; ').find(row => row.startsWith('ksweddingviewer_language=')).split('=')[1] || "english";
+	let currentLanguage = document.cookie.split('; ').find(row => row.startsWith('ksweddingviewer_language=')).split('=')[1] || "english"
 
-	const 	source 		= currentLanguage == "english" ? English : Italian;
-	const 	sections 	= key.split('.');
-	let 	result 		= source;
+	const 	source 		= currentLanguage == "english" ? English : Italian
+	const 	sections 	= key.split('.')
+	let 	result 		= source
 
 	for (const section of sections) {
 		if (!result[section]) {
-			return key;
+			return key
 		}
-		result = result[section];
+		result = result[section]
 	}
 
-	return typeof result === 'string' ? result : key;
+	return <>{ typeof result === 'string' ? parse(result) : key }</>
+}
+
+function parse(string) {
+	if (string.search("{newline}") !== -1)
+		string = create_newlines(string)
+
+	string = create_links(string)
+
+	return string
+}
+
+function create_newlines(string) {
+	let pos
+	let sub_string = string
+	let new_string = []
+	
+	while ((pos = sub_string.search("{newline}")) !== -1) {
+		new_string.push(sub_string.substring(0, pos))
+		new_string.push([<br key={Math.random().toString()}/>, <br key={Math.random().toString()}/>])
+		sub_string = sub_string.substring(pos + "{newline}".length, sub_string.length)
+	}
+
+	new_string.push(sub_string)
+
+	return new_string
+}
+
+function create_links(string_list) {
+	if (!Array.isArray(string_list)) return string_list
+
+	return string_list.map(entry => {
+		if (typeof(entry) !== 'string') return entry
+
+		let pos
+		let sub_string = entry
+		let new_string = []
+	
+		while ((pos = sub_string.search("{link")) !== -1) {
+			const end = sub_string.search("}")
+	
+			const for_regex = sub_string.substring(0, end + 1) 
+
+			const link = for_regex.match("(?<={link to=')(.*)(?='\ text=)")
+			const text = for_regex.match("(?<=text=')(.*)(?='})")
+	
+			new_string.push(sub_string.substring(0, pos))
+			
+			new_string.push(<a href={link[0]} key={Math.random().toString()}>{text[0]}</a>)
+			sub_string = sub_string.substring(end + 1, sub_string.length)
+		}
+
+		new_string.push(sub_string)
+
+		return new_string
+	})
 }
 
 // export { Translator };
