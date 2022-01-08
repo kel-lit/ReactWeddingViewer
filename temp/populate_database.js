a42d37e34b0f746fa272546403530b989f64e475
kel-lit <kel_littley@live.co.uk>
2022-01-08 21:58:58 +0000

    feat (Backend): Add gift api router

--- temp/populate_database.js
+++ temp/populate_database.js
@@ -1,21 +1,23 @@
 const initDb = require("../express/database/database").initDb
 const getDb = require("../express/database/database").getDb
-const guests = require("./guests.json")
+const guests = require("./guests2.json")
 
 used_codes = []
 
 guests_and_token = []
 
-function populate() {
+async function populate(_db) {
 	dayGuests = guests.allDay
 	evening = guests.evening
 
+	
 	toAddDay = get_list(dayGuests, true)
 	toAddEvening = get_list(evening, false)
+	toAddAll = toAddDay.concat(toAddEvening)
+	
+	const users = _db.collection("users")
 
-	const users = getDb().collection("users")
-
-	console.log(users)
+	response = await users.insertMany(toAddAll)
 }
 
 function get_list(guests, isDayGuest) {
@@ -43,13 +45,14 @@ function generate_code(names) {
 			"p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 	uppercase = lowercase.map(letter => letter.toUpperCase())
 	nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+	chars = ["-", "+", "@", "?"]
 
-	all = lowercase.concat(uppercase).concat(nums)
+	all = lowercase.concat(uppercase).concat(nums).concat(chars)
 
 	generate_token = () => {
 		token = ""
 
-		for (let i = 0; i < 8; i++) {
+		for (let i = 0; i < 10; i++) {
 			token += all[Math.floor(Math.random() * all.length)]
 		}
 
@@ -66,16 +69,15 @@ function generate_code(names) {
 	return token
 }
 
-initDb((err, db_) => {
-	console.log("Hello")
+async function update_token(db_) {
+	users = db_.collection('users')
+}
 
+initDb(async (err, _db) => {
+	if (err) throw err
 
-	if (err) console.log(err)
-	else console.log(db_)
+	await populate(_db)
+
+	process.exit()
 })
 
-populate()
-
-//generate_code()
-
-console.log(guests_and_token)
