870e07ab04ae33d211517f40a9f323408814fb85
kel-lit <kel_littley@live.co.uk>
2022-01-08 21:56:32 +0000

    feat (Home): add gift section to home page

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState, useContext } from 'react';
 import PageLayout, { PageHeading, PageSubHeading, PageContent, PageDivider } from '../AddOns/PageLayout';
 import NameJoiner from 'utils/NameJoiner';
+import { Button } from 'utils/Buttons'
+import useJsonApi from 'utils/useJsonApi'
 
 import { UserContext } from '../../index';
 
@@ -8,6 +10,12 @@ import styles from './Home.scss';
 
 export default function Home () {
 	const context = useContext(UserContext);
+
+	const [response, loading, error, request] = useJsonApi('/api/gift')
+
+	const gift = () => {
+		window.location.assign('/giveagift')
+	}
 
 	return (
 		<PageLayout>
@@ -20,8 +28,27 @@ export default function Home () {
 			</PageSubHeading>
 
 			<PageContent>
-				{t('pages.home.maincontent')}
+				{t('pages.home.maincontent1')}
+				<br />
+				{t('pages.home.maincontent2')}
+				<br />
+				{t('pages.home.maincontent3')}
+				<br />
+				{t('pages.home.maincontent4')}
 			</PageContent>
+
+			<PageHeading value={t('pages.home.gifts')} />
+
+			<PageDivider />
+
+			<PageContent>
+				{t('pages.home.giftsmessage1')}
+				<br />
+				{t('pages.home.giftsmessage2')}
+			</PageContent>
+
+			<Button className={styles.giftButton} text={t('pages.home.giveagift')} onClick={gift} />
+
 		</PageLayout>
 	)
 }
