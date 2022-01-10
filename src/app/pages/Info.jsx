016aac8bfae3b92d454196541540168478d52d61
kel-lit <kel_littley@live.co.uk>
2022-01-10 18:32:54 +0000

    feat (Information page): Completed info page

--- src/app/pages/Info.jsx
+++ src/app/pages/Info.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useContext } from 'react'
 import TextArea from '../AddOns/TextArea'
-import PageLayout, { PageHeading, PageContent, PageDivider, PageContentImage } from '../AddOns/PageLayout'
+import PageLayout, { PageHeading, PageContent, PageDivider, PageContentImage, PageSubHeading } from '../AddOns/PageLayout'
 import useJsonApi from 'utils/useJsonApi'
 import { UserContext } from '../../index'
 import MessageHandler from 'utils/MessageHandler'
@@ -45,18 +45,49 @@ export default function(props) {
 			<PageDivider />
 
 			<PageContent>
-				{t('pages.info.timeinfo')}
+				<PageSubHeading>
+					{t('pages.info.timetitle')}
+				</PageSubHeading>
+
+				<TimeTable />
+			</PageContent>
+
+			<PageContent>
 				<PageContentImage src={imageCeremonyRoom} />
+			</PageContent>
 
+			<PageContent>
+				<PageSubHeading>
+					{t('pages.info.foodtitle')}
+				</PageSubHeading>
 				{t('pages.info.foodinfo')}
+				<br />
+				<FoodTable />
 
+				<br/>
+				{t('pages.info.foodevening')}
+			</PageContent>
+
+			<PageContent>
+				<PageSubHeading>
+					{t('pages.info.hoteltitle')}
+				</PageSubHeading>
 				{t('pages.info.hotelinfo')}
+			</PageContent>
 
+			<PageContent>
+				<PageSubHeading>
+					{t('pages.info.bandtitle')}
+				</PageSubHeading>
 				{t('pages.info.bandinfo')}
 
 				<PageContentImage src={imageBand} />
-				<a href={"https://www.jukeboxmk.com/"} target="newtab">{t('pages.info.bandinfolink')}</a>
-				
+			</PageContent>
+
+			<PageContent>
+				<PageSubHeading>
+					{t('pages.info.songrequeststitle')}
+				</PageSubHeading>
 				{t('pages.info.songrequests')}
 				<div className={styles.songRequestContainer}>
 					<TextArea className={styles.textArea} maxLength={200} onChange={setSongRequests} value={songRequests} placeholder={t('pages.info.songrequestsplaceholder')} />
@@ -66,5 +97,91 @@ export default function(props) {
 
 			<MessageHandler message={message || {}} close={() => setMessage(null)} />
 		</PageLayout>
+	)
+}
+
+function TimeTable() {
+	return (
+		<table className={styles.table}>
+			<tbody>
+				<tr>
+					<th>{t('pages.info.timeoneheading')}</th>
+					<td>{t('pages.info.timeone')}</td>
+				</tr>
+				<tr>
+					<th>{t('pages.info.timetwoheading')}</th>
+					<td>{t('pages.info.timetwo')}</td>
+				</tr>
+				<tr>
+					<th>{t('pages.info.timethreeheading')}</th>
+					<td>{t('pages.info.timethree')}</td>
+				</tr>
+				<tr>
+					<th>{t('pages.info.timefourheading')}</th>
+					<td>{t('pages.info.timefour')}</td>
+				</tr>
+				<tr>
+					<th>{t('pages.info.timefiveheading')}</th>
+					<td>{t('pages.info.timefive')}</td>
+				</tr>
+				<tr>
+					<th>{t('pages.info.timesixheading')}</th>
+					<td>{t('pages.info.timesix')}</td>
+				</tr>
+				<tr>
+					<th>{t('pages.info.timesevenheading')}</th>
+					<td>{t('pages.info.timeseven')}</td>
+				</tr>
+				<tr>
+					<th>{t('pages.info.timeeightheading')}</th>
+					<td>{t('pages.info.timeeight')}</td>
+				</tr>
+				<tr>
+					<th>{t('pages.info.timenineheading')}</th>
+					<td>{t('pages.info.timenine')}</td>
+				</tr>
+				<tr>
+					<th>{t('pages.info.timetenheading')}</th>
+					<td>{t('pages.info.timeten')}</td>
+				</tr>
+				<tr>
+					<th>{t('pages.info.timeelevenheading')}</th>
+					<td>{t('pages.info.timeeleven')}</td>
+				</tr>
+			</tbody>
+		</table>
+	)
+}
+
+function FoodTable() {
+	return (
+		<table className={styles.table}>
+			<tbody>
+				<tr>
+					<th></th>
+					<th>{t('pages.info.foodmeateaters')}</th>
+					<th>{t('pages.info.foodveggie')}</th>
+					<th>{t('pages.info.foodvegan')}</th>
+					<th>{t('pages.info.foodkids')}</th>
+				</tr>
+				<tr>
+					<th>{t('pages.info.foodstarterheading')}</th>
+					<td>{t('pages.info.foodstarter')}</td>
+					<td>{t('pages.info.foodveggiestarter')}</td>
+					<td>{t('pages.info.foodveganstarter')}</td>
+					<td>{t('pages.info.foodkidsstarter')}</td>
+				</tr>
+				<tr>
+					<th>{t('pages.info.foodmainheading')}</th>
+					<td>{t('pages.info.foodmain')}</td>
+					<td colSpan={2}>{t('pages.info.foodveggiemain')}</td>
+					<td>{t('pages.info.foodkidsmain')}</td>
+				</tr>
+				<tr>
+					<th>{t('pages.info.fooddessertheading')}</th>
+					<td colSpan={4}>{t('pages.info.fooddessert')}</td>
+				</tr>
+			</tbody>
+		</table>
 	)
 }
